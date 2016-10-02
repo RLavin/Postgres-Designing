751150b73b71da92e6fcf76fd504fb94a352a878
Raul <Raul@Rauls-MacBook-Pro.local>
2016-10-02 17:21:01 -0400

    One more time lol

--- src/main/resources/myselectstatementssql.sql
+++ src/main/resources/myselectstatementssql.sql
@@ -1,8 +1,27 @@
+
+SELECT * FROM public.companies c
+LEFT OUTER JOIN  public.employees
+  ON c.com_id=employees.emp_com_id
+
+
+
+
+
+SELECT * FROM public.companies c
+LEFT OUTER JOIN  public.address
+  ON c.com_id=address.add_com_id
+
+
+
+
+
 SELECT * FROM public.customers c
 LEFT OUTER JOIN public.join_customers_orders jco
     ON c.cus_id=jco.jco_cus_id
     LEFT OUTER JOIN public.orders o
     ON o.ord_id=jco.jco_ord_id;
+
+
 
 SELECT * FROM public.products p
 LEFT OUTER JOIN public.join_products_orders jpo
@@ -11,11 +30,3 @@ LEFT OUTER JOIN public.join_products_orders jpo
     ON o.ord_id=jpo.jpo_ord_id;
 
 
-
-SELECT * FROM public.companies c
-LEFT OUTER JOIN  public.employees
-  ON c.com_id=employees.emp_com_id
-
-SELECT * FROM public.companies c
-LEFT OUTER JOIN  public.address
-  ON c.com_id=address.add_com_id
--- src/main/resources/mytablessql.sql
+++ src/main/resources/mytablessql.sql
@@ -1,22 +1,3 @@
-CREATE TABLE address
-(
-    add_id INTEGER PRIMARY KEY NOT NULL,
-    add_name VARCHAR(255),
-    add_phone_number VARCHAR(155),
-    add_1_line VARCHAR(255),
-    add_2_line VARCHAR(255),
-    add_city VARCHAR(155),
-    add_state VARCHAR(155),
-    add_zip INTEGER,
-    add_com_id INTEGER,
-    add_ord_id INTEGER,
-    add_cus_id INTEGER,
-    add_emp_id INTEGER,
-    CONSTRAINT address_companies_com_id_fk FOREIGN KEY (add_com_id) REFERENCES companies (com_id),
-    CONSTRAINT address_orders_ord_id_fk FOREIGN KEY (add_ord_id) REFERENCES orders (ord_id),
-    CONSTRAINT address_customers_cus_id_fk FOREIGN KEY (add_cus_id) REFERENCES customers (cus_id),
-    CONSTRAINT address_employees_emp_id_fk FOREIGN KEY (add_emp_id) REFERENCES employees (emp_id)
-);
 
 CREATE TABLE companies
 (
@@ -83,4 +64,24 @@ CREATE TABLE join_products_orders
     jpo_ord_id INTEGER,
     CONSTRAINT join_products_orders_products_pro_id_fk FOREIGN KEY (jpo_pro_id) REFERENCES products (pro_id),
     CONSTRAINT join_products_orders_orders_ord_id_fk FOREIGN KEY (jpo_ord_id) REFERENCES orders (ord_id)
+);
+
+CREATE TABLE address
+(
+    add_id INTEGER PRIMARY KEY NOT NULL,
+    add_name VARCHAR(255),
+    add_phone_number VARCHAR(155),
+    add_1_line VARCHAR(255),
+    add_2_line VARCHAR(255),
+    add_city VARCHAR(155),
+    add_state VARCHAR(155),
+    add_zip INTEGER,
+    add_com_id INTEGER,
+    add_ord_id INTEGER,
+    add_cus_id INTEGER,
+    add_emp_id INTEGER,
+    CONSTRAINT address_companies_com_id_fk FOREIGN KEY (add_com_id) REFERENCES companies (com_id),
+    CONSTRAINT address_orders_ord_id_fk FOREIGN KEY (add_ord_id) REFERENCES orders (ord_id),
+    CONSTRAINT address_customers_cus_id_fk FOREIGN KEY (add_cus_id) REFERENCES customers (cus_id),
+    CONSTRAINT address_employees_emp_id_fk FOREIGN KEY (add_emp_id) REFERENCES employees (emp_id)
 );
